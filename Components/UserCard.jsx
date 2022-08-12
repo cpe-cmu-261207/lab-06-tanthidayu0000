c4adb6cb26ec4eb334093e92fda2a1827f369642
thidayu <tanthidayu_0000@hotmail.com>
2022-08-12 14:36:35 +0700

    usc finished

---
+++ Components/UserCard.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons";
+import UserCardDetail from "./UserCardDetail";
+
+export default function UserCard() {
+  const [click, setClick] = useState(false);
+  return (
+    <div
+      className="border-bottom"
+      onClick={() => {
+        click ? setClick(false) : setClick(true);
+      }}
+    >
+      <div className="d-flex align-item-canter p-3">
+        <img src={props.imgUrl} width="90px" class="rounded-circle me-4" />
+        <span className="text-center display-6 me-auto">{props.name}</span>
+        {!click && <IconChevronDown />}
+        {!click && <IconChevronUp />}
+      </div>
+      {click && <UserCardDetail email={props.email} address={props.address} />}
+    </div>
+  );
+}
